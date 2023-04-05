a08aace0efdde36fe41a280a18642250f249c241
ElAbdelali <eljaouhariabdelali@outlook.com>
2023-04-05 15:33:58 -0500

    Completed the while loop, called and printed the functions from arithmetic.

--- calculator.py
+++ calculator.py
@@ -9,25 +9,27 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 queue = True
 
 while queue:
-    token = input("input an equation or press q to quit(seperate the operators and operands by a space)")
+    token = input('Please enter either [square, cube, power, %, /, *, +, -] followed by values seperated by spaces or press q to quit: ')
     token_list = token.split(' ')
     
     if token == 'q':
         print("Thank you for using the calculator!") 
         queue = False
     elif token_list[0] == 'square':
-        print(float(token_list[1]) ** float(token_list[2]))
+        print(square(float(token_list[1])))
     elif token_list[0] == "cube":
-        print(float(token_list[1]) + float(token_list[2]))
+        print(cube(float(token_list[1])))
     elif token_list[0] == 'power':
-        print(float(token_list[1]) ** float(token_list[2]))
+        print(power(float(token_list[1], float(token_list[2]))))
     elif token_list[0] == "%":
-        print(float(token_list[1]) + float(token_list[2]))
+        print(mod(float(token_list[1]), float(token_list[2])))
     elif token_list[0] == '/':
-        print(float(token_list[1]) ** float(token_list[2]))
+        print(divide(float(token_list[1]), float(token_list[2])))
     elif token_list[0] == "*":
-        print(float(token_list[1]) + float(token_list[2]))
+        print(multiply(float(token_list[1]), float(token_list[2])))
     elif token_list[0] == '+':
-        print(float(token_list[1]) ** float(token_list[2]))
+        print(add(float(token_list[1]), float(token_list[2])))
     elif token_list[0] == "-":
-        print(float(token_list[1]) + float(token_list[2]))
+        print(subtract(float(token_list[1]), float(token_list[2])))
+    else:
+        continue
